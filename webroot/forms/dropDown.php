3fe3f1cd215a23584ea3587330a18e35191fe665
Scott Francis <sgf14@netzero.com>
2014-10-26 13:55:53 -0600

    chap 12 form handling/system upgrade to win 8.1 test

--- webroot/forms/dropDown.php
+++ webroot/forms/dropDown.php
@@ -7,11 +7,30 @@ echo <<<_END
     </head>
     <body>
         Vegetables
+        <!-- default setup-->
         <select name="veg" size="1">
         <option value="Peas">Peas</option>
         <option value="Carrots">Carrots</option>
         <option value="Cabbage">Cabbage</option>
         <option value="Rutabegas">Rutabegas</option>
+        </select>
+        <br>
+        <br>
+
+        Fruits
+        <!-- see pg 289, can select multiple selections if needed, but usually a set of checkboxes is 
+        more intuitive to the user.  'multiple' being present is sufficient, if the word is not there its 
+        single selection.  see W3C schools, html ref, select tags-->
+
+        <select name="veg" size="4" multiple>
+        <option value="Oranges">Oranges</option>
+
+        <!-- can default select an item in the list as coded below the "selected" part isnt technically 
+        required, but is advised so yo can reference it by name elsewhere in code -->
+
+        <option selected="selected" value="Apples">Apples</option>
+        <option value="Blueberries">Blueberries</option>
+        <option value="Bananas">Bananas</option>
         </select>
     </body>
 </html>
